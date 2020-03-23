499a6ae913a2cbecbd08b363f921e02ce0868b8f
wanglei105 <690830629@qq.com>
2020-03-23 11:06:50 +0800

    add server

--- tortoise-client/src/main/java/com/wanglei/business/NIOClient.java
+++ tortoise-client/src/main/java/com/wanglei/business/NIOClient.java
@@ -1,4 +1,34 @@
 package com.wanglei.business;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
 public class NIOClient {
+
+    private static final int PORT = 8888;
+
+
+    public static void initClient() throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(PORT);
+        socketChannel.bind(inetSocketAddress);
+        socketChannel.connect(inetSocketAddress);
+        ByteBuffer byteBuffer=ByteBuffer.allocate(2048);
+        while(true){
+            Scanner scanner=new Scanner(System.in);
+            String context=scanner.nextLine();
+            byteBuffer.put(context.getBytes());
+            socketChannel.write(byteBuffer);
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        NIOClient.initClient();
+    }
 }
--- tortoise-server/src/main/java/com/wanglei/business/NIOServer.java
+++ tortoise-server/src/main/java/com/wanglei/business/NIOServer.java
@@ -1,4 +1,52 @@
 package com.wanglei.business;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 public class NIOServer {
+
+    private static final int PORT = 8888;
+
+
+    public static void initServer() throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(PORT);
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.bind(inetSocketAddress);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (selector.select() > 0) {
+            Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+            Iterator<SelectionKey> selectionKeyIterator = selectionKeySet.iterator();
+            while (selectionKeyIterator.hasNext()) {
+                SelectionKey selectionKey = selectionKeyIterator.next();
+                selectionKeyIterator.remove();
+                if (selectionKey.isAcceptable()) {
+                    System.out.println("客户端发起了连接");
+                    ServerSocketChannel serverSocketChannel1 = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel1.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                }
+                if (selectionKey.isReadable()) {
+                    System.out.println("客户端说了啥");
+                }
+            }
+
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        NIOServer.initServer();
+    }
+
+
 }
