d99572a5863f6a2e17ee5da45822115ba5bc62ee
王蕾 <wanglei@wangleideMacBook-Pro.local>
2021-01-16 14:09:18 +0800

    f

--- tortoise-util/src/main/java/com/wanglei/business/Cache/MyCache.java
+++ tortoise-util/src/main/java/com/wanglei/business/Cache/MyCache.java
@@ -4,5 +4,6 @@ public class MyCache {
     public static void main(String[] args) {
         System.out.println("this is a myCache");
         System.out.println("1");
+        System.out.println(3);
     }
 }
