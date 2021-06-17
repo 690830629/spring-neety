4003e8a8022566443a9e5eb68860aa695216f166
王蕾 <wanglei@wangleideMacBook-Pro.local>
2021-06-17 08:45:06 +0800

    add log

---
+++ tortoise-log/tortoise-log4j2/src/main/java/com/wanglei/business/MyLog.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2020 iQIYI.COM - All Rights Reserved.
+ * This file is part of Pizza project. Unauthorized copy
+ * of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ *
+ * Author(s): INQ Team <iig_ipd_inq@qiyi.com>
+ */
+package com.wanglei.business;
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class MyLog {
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(MyLog.class);
+        logger.info("this is a log4j2 demo");
+
+
+    }
+}
